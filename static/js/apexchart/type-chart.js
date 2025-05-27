f4828ae5089809c9124f092a095b989b1b88adc9
unknown <lourdes123duarte@gmail.com>
2025-05-27 12:28:15 -0300

    changes

--- static/js/apexchart/type-chart.js
+++ static/js/apexchart/type-chart.js
@@ -3,18 +3,18 @@
 function area_chart(id, intermensual_data, interanual_data,meses, titulo_chart, max_chart, min_chart)
 {
    
-  var options = {
-    series: [
+ var options = {
+  series: [
     {
       name: "Intermensual",
-      data:  intermensual_data
+      data: intermensual_data
     },
     {
       name: "Interanual",
       data: interanual_data
     }
   ],
-    chart: {
+  chart: {
     height: 350,
     type: 'line',
     dropShadow: {
@@ -32,36 +32,63 @@ function area_chart(id, intermensual_data, interanual_data,meses, titulo_chart, 
       show: true
     }
   },
-  colors: ['#007D9D', '#859222'],
+  colors: ['#003764', '#859222'], // Tus colores actuales
 
   dataLabels: {
-    enabled: true,
+    enabled: false, // Deshabilitado como lo pediste
   },
   stroke: {
-    curve: 'smooth'
+    curve: 'smooth', // Hace que la línea sea suave, como en la imagen
+    width: 2,
   },
   title: {
     text: titulo_chart,
     align: 'left'
   },
   grid: {
-    borderColor: '#e7e7e7',
+    show: true,
+    borderColor: '#e7e7e7', // Color de borde para la rejilla
+    strokeDashArray: 0, // No usar líneas punteadas para la rejilla
+    position: 'back', // La rejilla detrás de las líneas del gráfico
     row: {
-      colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+      colors: ['#f3f3f3', 'transparent'], // Toma un array que se repetirá en las filas
       opacity: 0.5
     },
+    xaxis: {
+      lines: {
+        show: false // Ocultar las líneas verticales de la rejilla
+      }
+    },
+    yaxis: {
+        lines: {
+            show: true,
+            colors: ['#e7e7e7'] // Color muy claro para las líneas horizontales
+        }
+    }
   },
   markers: {
-    size: 1
+    size: 5,        // Tamaño de los círculos en los puntos de datos (estos son los "puntos" que quieres ver)
+    colors: ['#007D9D', '#859222'], // Colores que coinciden con tus series
+    strokeColors: '#fff', // Color del borde de los marcadores (blanco para que resalten)
+    strokeWidth: 2,       // Ancho del borde de los marcadores
+    hover: {
+      sizeOffset: 2 // Los marcadores pueden crecer un poco al pasar el ratón
+    }
   },
   xaxis: {
-    categories: meses,
+    categories: meses, // Tus categorías de meses
     title: {
       text: 'Meses'
+    },
+    tooltip: {
+      enabled: true, // Habilita el tooltip en el eje X
+      formatter: function (val, { series, seriesIndex, dataPointIndex, w }) {
+        // Asegúrate de que el valor sea el nombre del mes de tus categorías
+        return w.globals.categoryLabels[dataPointIndex];
+      }
     }
   },
   yaxis: {
-    
     min: min_chart,
     max: max_chart
   },
@@ -75,15 +102,38 @@ function area_chart(id, intermensual_data, interanual_data,meses, titulo_chart, 
   annotations: {
     yaxis: [{
       y: 0,
-      borderColor: '#000000',  // Color de la línea
-      strokeDashArray: 0,       // Línea punteada
-      
+      borderColor: '#000000', // Color de la línea
+      strokeDashArray: 0, // Línea sólida (0 para no punteada)
     }]
+  },
+  tooltip: {
+    enabled: true, // Habilitar el tooltip en general
+    x: {
+      show: true,
+      formatter: function(val, { series, seriesIndex, dataPointIndex, w }) {
+        // Muestra el nombre del mes de las categorías en la parte superior del tooltip
+        return w.globals.categoryLabels[dataPointIndex];
+      }
+    },
+    y: {
+      formatter: function (val) {
+        // Muestra el valor como un porcentaje en el tooltip
+        if (typeof val === 'number') {
+            return val.toFixed(1) + '%'; // Formatea a un decimal y añade '%'
+        }
+        return val;
+      },
+      title: {
+          formatter: function (seriesName) {
+              return seriesName; // Muestra el nombre de la serie (ej. "Intermensual")
+          }
+      }
+    }
   }
-  };
+};
 
-  var chart = new ApexCharts(document.querySelector('#'+id), options);
-  chart.render();
+var chart = new ApexCharts(document.querySelector('#' + id), options);
+chart.render();
 }
 
 
